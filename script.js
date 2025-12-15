14b5b0904657525e5633cefeb7d1c89b135b7cdf
Z Sarkissian <51246992+diverdrown@users.noreply.github.com>
2025-12-15 15:15:39 -0600

    adding more things

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-body {
-    background-color: lightblue;
-}
+// Load header into all pages
+fetch('../portfolio/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    });
