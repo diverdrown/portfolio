7272500e609b903a6682bb6abfeb7a300986b1ce
Z Sarkissian <51246992+diverdrown@users.noreply.github.com>
2025-12-15 15:34:23 -0600

    fixyfix

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Load header into all pages
-fetch('../portfolio/header.html')
+const headerPath = window.location.pathname.includes('/projects/') ? '../header.html' : 'header.html';
+fetch(headerPath)
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
